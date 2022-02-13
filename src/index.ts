5365e5538c30b04d86f4f0e4d16f9663ab023409
Carlos Moran <moran_krlos@hotmail.com>
2022-02-12 18:34:19 -0600

    connection to the database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./config/user.routes";
+import { createConnection } from "typeorm";
 
 const app = express();
+createConnection();
 const port = 3000;
+
+// Middlewares
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`Server on port ${port}`);
